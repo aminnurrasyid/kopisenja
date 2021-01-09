e0352a2780cb9f9875413f36b6e2ff29d323d3f9
aminnurrasyid <57680454+aminnurrasyid@users.noreply.github.com>
2021-01-09 13:49:20 +0800

    Add files via upload

---
+++ 2-menu/menu.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    //buttons in navigation bar
+    $("#NavhomeBtn").click(function(){   
+        location.href="../index.html"; 
+    });
+    $("#NavmenuBtn").click(function(){   
+        location.href="menu.html"; 
+    });
+    $("#NavnewsBtn").click(function(){   
+        location.href="index.html";         //unchanged
+    });
+    $("#NavaboutBtn").click(function(){   
+        location.href="index.html";         //unchanged
+    });
+
+    //button in footer
+    $("#sendfbackBtn").click(function(){
+        location.href="index.html";          //unchanged
+    });
+});
