22071dd90e29ae719e0986ade1488ed8d73bbb08
aminnurrasyid <57680454+aminnurrasyid@users.noreply.github.com>
2021-01-11 01:07:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 
     //button in footer
     $("#sendfbackBtn").click(function(){
-        location.href="./5-feedback/feedback.html"; 
+        location.href="5-feedback/feedback.html"; 
     });
 });
 
