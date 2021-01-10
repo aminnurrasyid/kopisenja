17f0f0c7d1211f4dc44ff0985cc815245a0dbf86
aminnurrasyid <57680454+aminnurrasyid@users.noreply.github.com>
2021-01-10 13:08:10 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    //buttons in navigation bar
+    $("#NavhomeBtn").click(function(){   
+        location.href="index.html"; 
+    });
+    $("#NavmenuBtn").click(function(){   
+        location.href="2-menu/menu.html"; 
+    });
+    $("#NavnewsBtn").click(function(){   
+        location.href="3-news/news.html"; 
+    });
+    $("#NavaboutBtn").click(function(){   
+        location.href="4-about/about.html"; 
+    });
+
+    //button in second fold
+    $("#readnewsBtn").click(function(){
+        location.href="3-news/news.html";
+    });
+    //button in third fold
+    $("#browsemenuBtn").click(function(){
+        location.href="2-menu/menu.html"; 
+    });
+
+    //button in footer
+    $("#sendfbackBtn").click(function(){
+        location.href="./5-feedback/feedback.html"; 
+    });
+});
+
+function closePromo() {
+    document.querySelector('.promotion').style.display='none';
+}
